0017288c747a1e3847bf7cd6c4f10e41f12fa396
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-11-10 10:18:20 +0600

    Sidebar update

--- forntend/src/layout/AppSidebar.tsx
+++ forntend/src/layout/AppSidebar.tsx
@@ -562,7 +562,6 @@ const AppSidebar: React.FC = () => {
             </div>
           </div>
         </nav>
-        {isExpanded || isHovered || isMobileOpen ? <SidebarWidget /> : null}
       </div>
     </aside>
   );
