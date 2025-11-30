13a1473b411b6724c33bd7edc4b473648c2bb2c1
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-11-30 18:05:37 +0600

    User Profile Updated

--- frontend/src/components/user-profile/UserMetaCard.tsx
+++ frontend/src/components/user-profile/UserMetaCard.tsx
@@ -6,10 +6,12 @@ import Button from "../ui/button/Button";
 import Input from "../form/input/InputField";
 import Label from "../form/Label";
 import Image from "next/image";
+import { useAuth } from "@/context/AuthContext";
 
 
 export default function UserMetaCard() {
   const { isOpen, openModal, closeModal } = useModal();
+   const {user, roles} = useAuth();
   const handleSave = () => {
     // Handle save logic here
     console.log("Saving changes...");
@@ -30,11 +32,11 @@ export default function UserMetaCard() {
             </div>
             <div className="order-3 xl:order-2">
               <h4 className="mb-2 text-lg font-semibold text-center text-gray-800 dark:text-white/90 xl:text-left">
-                Musharof Chowdhury
+                {user?.name}
               </h4>
               <div className="flex flex-col items-center gap-1 text-center xl:flex-row xl:gap-3 xl:text-left">
                 <p className="text-sm text-gray-500 dark:text-gray-400">
-                  Team Manager
+                  {roles?.map(role => role).join(", ")}
                 </p>
                 <div className="hidden h-3.5 w-px bg-gray-300 dark:bg-gray-700 xl:block"></div>
                 <p className="text-sm text-gray-500 dark:text-gray-400">
