81a8f7e9a392e52966ad671b7dedf2aa7ed1f089
shekhmanzurelahishadhin <shadhinmonzur18@gmail.com>
2025-12-06 13:41:50 +0600

    small changes on permission

--- frontend/src/app/(admin)/authorization/permissions/page.tsx
+++ frontend/src/app/(admin)/authorization/permissions/page.tsx
@@ -1,6 +1,10 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from 'react'
 
 export default function Permission() {
+      useEffect(() => {
+        document.title = "Permission Management | Inventory Management System";
+      }, []);
   return (
     <div>page</div>
   )
